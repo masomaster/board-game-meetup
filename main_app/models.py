449c235f7eae1d25ad4ef7e1fb3894c86988d727
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-10-28 13:39:57 -0700

    made game model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Game (models.Model):
+    name = models.CharField(max_length=50)
+    min_player = models.IntegerField()
+    max_player = models.IntegerField()
+    avg_game_play = models.IntegerField()
+    difficulty_rating = models.IntegerField()
+    genre = models.CharField(max_length=50)
+    min_age = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.name
+
