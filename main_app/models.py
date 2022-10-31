27a26cffd947786b1b036c311417e0fea37482c5
nukaterrapin7 <pythonism007@gmail.com>
2022-10-31 12:30:05 -0500

    Adds new game function

--- main_app/models.py
+++ main_app/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
 class Game (models.Model):
     name = models.CharField(max_length=50)
     min_player = models.IntegerField()
-    max_player = models.IntegerField(null=True)
+    max_player = models.IntegerField(blank= True, null=True)
     avg_game_play = models.IntegerField()
-    difficulty_rating = models.IntegerField(null=True)
-    genre = models.CharField(max_length=50, null=True)
+    difficulty_rating = models.IntegerField(blank= True, null=True)
+    genre = models.CharField(max_length=50, blank= True)
     min_age = models.IntegerField(default=5)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField(max_length=250, null=True)
+    description = models.TextField(max_length=250, blank= True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('games_detail', kwargs={'game_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import  CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 
 from django.views.generic import ListView
 
@@ -33,3 +37,14 @@ def games_index(request):
 def games_detail(request, game_id):
   game = Game.objects.get(id=game_id)
   return render(request, 'games/games_detail.html', {'game': game})
+
+class GameCreate(LoginRequiredMixin, CreateView):
+  model = Game
+  fields = [
+    'name', 'min_player', 'max_player', 'avg_game_play', 'difficulty_rating', 'genre', 'min_age', 'description'
+  ]
+
+  def form_valid(self, form):
+    print('this got to view function')
+    form.instance.user = self.request.user
+    return super().form_valid(form)
