4a98b9bc8cd01377ad3eb2c631fb249e9307ea21
Jessicanguyenn81 <Jessicanguyenn81@gmail.com>
2022-11-03 12:03:08 -0700

    views

--- main_app/views.py
+++ main_app/views.py
@@ -117,19 +117,14 @@ def leave_meeting(request, meeting_id):
     return redirect('meetings_index')
 
 def add_photo(request, game_id):
-    # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
         try:
             bucket = os.environ['S3_BUCKET']
             s3.upload_fileobj(photo_file, bucket, key)
-            # build the full url string
             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
             Photo.objects.create(url=url, game_id=game_id)
         except Exception as e:
             print('An error occurred uploading file to S3')
@@ -137,19 +132,14 @@ def add_photo(request, game_id):
     return redirect('games_detail', game_id=game_id)
 
 def add_meeting_photo(request, meeting_id):
-    # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        # just in case something goes wrong
         try:
             bucket = os.environ['S3_BUCKET']
             s3.upload_fileobj(photo_file, bucket, key)
-            # build the full url string
             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
-            # we can assign to cat_id or cat (if you have a cat object)
             Photo.objects.create(url=url, meeting_id=meeting_id)
         except Exception as e:
             print('An error occurred uploading file to S3')
